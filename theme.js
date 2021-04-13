1035ec0b47d2d7365f6cc56e47208791d32ee1c6
Musa Sutisna <mus4.sutisn4@gmail.com>
2021-04-13 13:08:39 +0700

    Add theme author to theme

--- theme.js
+++ theme.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
   'theme_name': String,
   'theme_version': String,
-  'theme_url': String
+  'theme_url': String,
+  'theme_author': String
 });
 
 module.exports = mongoose.model('theme', schema, 'themes');
