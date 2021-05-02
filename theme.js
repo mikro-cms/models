01313b7dc32e5c2eaae1c575f6ceee10d1ae515d
Musa Sutisna <mus4.sutisn4@gmail.com>
2021-05-02 15:03:40 +0700

    Add complete theme info

--- theme.js
+++ theme.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-  'theme_path': String,
-  'theme_info': String,
-  'theme_path_options': String,
-  'theme_view': String,
-  'theme_public': String,
-  'theme_public_url': String,
   'theme_name': String,
   'theme_version': String,
+  'theme_author': String,
   'theme_url': String,
-  'theme_author': String
+  'theme_path': String,
+  'theme_info': String,
+  'theme_options': String,
+  'theme_view': String,
+  'theme_public_path': String,
+  'theme_public_url': String
 }, {
   autoCreate: true
 });
