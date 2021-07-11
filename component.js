d9d0cfabcea08564eb1a3f236b95d7ba9d7622ef
Musa Sutisna <mus4.sutisn4@gmail.com>
2021-07-11 09:22:57 +0700

    Quoted component schema

--- component.js
+++ component.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-  component_name: String,
-  component_options: Object,
-  page: { type: mongoose.Schema.Types.ObjectId, ref: 'page', default: null },
+  'component_name': String,
+  'component_options': Object,
+  'page': { type: mongoose.Schema.Types.ObjectId, ref: 'page', default: null },
 }, {
   autoCreate: true,
   minimize: false
